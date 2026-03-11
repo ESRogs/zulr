f36e495920d1454385b659858c08af2f796d6886
ESRogs <esrogs@gmail.com>
2026-03-11 10:53:12 -0700

    add SuccessResponseSchema and emoji reaction API functions

--- packages/zulip-ts/src/index.ts
+++ packages/zulip-ts/src/index.ts
@@ -12,8 +12,10 @@ export type {
   UpdateMessageParams,
 } from './messages.ts'
 export {
+  addReaction,
   getMessages,
   markAsRead,
+  removeReaction,
   sendDirectMessage,
   sendStreamMessage,
   updateMessage,
@@ -44,5 +46,14 @@ export type {
   UpdateMessageResponse,
 } from './schemas.ts'
 export type { CreateChannelParams, UpdateChannelParams } from './streams.ts'
-export { createChannel, getStreams, getTopics, subscribe, updateChannel } from './streams.ts'
-export { getMembers } from './users.ts'
+export {
+  archiveStream,
+  createChannel,
+  getStreams,
+  getSubscribers,
+  getTopics,
+  subscribe,
+  updateChannel,
+} from './streams.ts'
+export type { UpdateStatusParams } from './users.ts'
+export { getMembers, updateStatus } from './users.ts'
